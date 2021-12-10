2cf1e077be58b054ddbfc30db8adf21e26278e00
vaish9navi <78014334+vaish9navi@users.noreply.github.com>
2021-12-11 01:07:12 +0530

    Adding Search Gui and Agent

---
+++ src/bookstore/Search.java
@@ -0,0 +1,119 @@
+//Search GUI Page
+
+package bookstore;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class Search extends JFrame {
+
+	private JPanel panel1;
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField searchKeyword;
+	private JTextField username;
+	UserManagerAgent UserManagerAgent = new UserManagerAgent();
+	private JLabel registerLabel;
+	private JPasswordField password;
+	JFrame jFrame;
+
+	public Search(SearchAgent SearchAgent) {
+		System.out.print("in Search page");
+		JPanel container = new JPanel();
+
+		container.setLayout(new CardLayout(0, 0));
+		this.jFrame = new JFrame();
+		 this.jFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+	            @Override
+	            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+	                super.windowClosing(windowEvent);
+	                SearchAgent.killAgent(SearchAgent.getLocalName());
+	            }
+	        });
+		 this.jFrame.setSize(400, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+	
+
+		
+		JPanel searchPanel = new JPanel();
+		
+		
+		JPanel panel = new JPanel();
+		searchPanel.add(panel);
+		
+		JLabel searchLabel = new JLabel("Enter keyword");
+		panel.add(searchLabel);
+		
+		searchKeyword = new JTextField();
+		panel.add(searchKeyword);
+		searchKeyword.setColumns(10);
+		
+		JButton searchButton = new JButton("Lookup");
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				table.setModel(SearchAgent.searchBooks(searchKeyword.getText()));
+			}
+		});
+		panel.add(searchButton);
+		
+		JLabel results = new JLabel("Searched results");
+		results.setAlignmentX(Component.CENTER_ALIGNMENT);
+		searchPanel.add(results);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		searchPanel.add(scrollPane);
+		
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int column = 0;
+				int row = table.getSelectedRow();
+				String ISBN = table.getModel().getValueAt(row, column).toString();
+				SearchAgent.populateDetailsPane(ISBN,SearchAgent);
+				CardLayout cl = (CardLayout)(container.getLayout());
+			    cl.show(container, "detailsPanel");
+			}
+		});
+
+		scrollPane.setViewportView(table);
+		table.setModel(SearchAgent.getbooks());
+		
+		JPanel searchResultsPanel = new JPanel();
+		searchPanel.add(searchResultsPanel);
+		this.jFrame.add(searchPanel);
+		CardLayout cl = (CardLayout)(container.getLayout());
+	    cl.show(container, "searchPanel");
+		
+		this.jFrame.setVisible(true);
+	}
+	
+	
+
+	
+	
+
+}
--- src/bookstore/SearchAgent.java
+++ src/bookstore/SearchAgent.java
@@ -1,7 +1,11 @@
+//Search Agent Page
+
 package bookstore;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JLabel;
+import javax.swing.JTable;
 import javax.swing.table.TableModel;
 
 import net.proteanit.sql.DbUtils;
@@ -11,25 +15,103 @@ import jade.core.AgentContainer;
 import jade.core.behaviours.*;
 import jade.wrapper.AgentController;
 
-public class SearchAgent extends Agent{
-	public TableModel getbooks() throws SQLException {
+public class SearchAgent extends MainAgent{
+	JLabel detailsGenre;
+	JLabel detailsISBN;
+	JLabel detailsTitle;
+	JLabel detailsAuthor;
+	JLabel detailsDescription;
+	JLabel detailsPublication;
+	JLabel detailsQuantity;
+	JLabel detailsPrice;
+	JLabel detailsDateAdded;
+	private JTable table_1;
+	 public SearchAgent() {
+		 System.out.println("in Search Agent");
+	 }  
+     
+	 protected void setup() 
+     {
+    	 System.out.println("Search-agent " + getAID().getName() + "is ready.");
+    	 new Search(this);
+
+         addBehaviour(new TickerBehaviour(this, Long.valueOf(10000)) {
+             protected void onTick() {
+                 System.out.println("UserManagers-agent " + getAID().getName() + "is cycling.");
+             }
+         });
+     }   //  --- setup ---
+	
+	 public TableModel getbooks()  {
 		DBConnect db = new DBConnect();
-    	ResultSet booksset = db.getbooks();
-    	return DbUtils.resultSetToTableModel(booksset);
-	}
-	public TableModel getbooksbykeyword(String keyword) throws SQLException {
-		DBConnect db = new DBConnect();
-    	ResultSet booksset = db.searchbookbykeyword(keyword);
-//    	System.out.print(DbUtils.resultSetToTableModel(booksset));
-    	return DbUtils.resultSetToTableModel(booksset);
-	}
-	public TableModel getbooksbyattribute(String name, String value) throws SQLException {
-		DBConnect db = new DBConnect();
-    	ResultSet booksset = db.getbooksbyattribute(name,value);
-//    	System.out.print(DbUtils.resultSetToTableModel(booksset));
-    	return DbUtils.resultSetToTableModel(booksset);
+    	ResultSet booksset;
+		try {
+			booksset = db.getbooks();
+			return DbUtils.resultSetToTableModel(booksset);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return DbUtils.resultSetToTableModel(null);
+		}
+    	
 	}
 	
+	 public TableModel searchBooks(String Keyword)  {
+		DBConnect db = new DBConnect();
+    	ResultSet booksset;
+		try {
+			booksset = db.searchBooks(Keyword);
+			return DbUtils.resultSetToTableModel(booksset);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return DbUtils.resultSetToTableModel(null);
+		}
+    	
+	}
+	
+	 public Book getBookFromISBN(String ISBN)  {
+		DBConnect db = new DBConnect();
+    	Book book;
+		try {
+			book = db.getBookFromISBN(ISBN);
+			return book;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return new Book();
+		}
+    	
+	}
+	 
+	 public TableModel getBookRowFromISBN(String ISBN)  {
+		 DBConnect db = new DBConnect();
+	    	ResultSet book;
+			try {
+				book = db.getBookRowFromISBN(ISBN);
+				return DbUtils.resultSetToTableModel(book);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return DbUtils.resultSetToTableModel(null);
+			}
+	    	
+		}
+	
+	 public void populateDetailsPane(String ISBN, SearchAgent searchAgent) {
+			Book book=searchAgent.getBookFromISBN(ISBN);
+			System.out.print(book);
+			detailsAuthor.setText(book.getAuthor());
+			detailsGenre.setText(book.getGenre());
+			detailsISBN.setText(book.getISBN());
+			detailsTitle.setText(book.getTitle());
+			detailsDescription.setText(book.getDescription());
+			detailsPublication.setText(book.getPublication());
+			detailsQuantity.setText(book.getQuantity());
+			detailsPrice.setText(book.getPrice());
+			detailsDateAdded.setText(book.getDateAdded());
+			
+		}
 	
 	
 }
